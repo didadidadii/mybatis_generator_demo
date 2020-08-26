d2e2a23625b6b65b50babf7bcac3fe2c239196cc
zs <123@qq.com>
2020-08-26 15:45:54 +0800

    mybatisis_generator 练习

---
+++ mybatis_generator/src/main/java/com/xulihao/generator/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.xulihao.generator.dao;
+
+import com.xulihao.generator.entity.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
---
+++ mybatis_generator/src/main/java/com/xulihao/generator/entity/User.java
@@ -0,0 +1,58 @@
+package com.xulihao.generator.entity;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private Integer id;
+
+    private String name;
+
+    private Integer age;
+
+    private String email;
+
+    private static final long serialVersionUID = 1L;
+
+    public User(Integer id, String name, Integer age, String email) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+    public User() {
+        super();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+}
